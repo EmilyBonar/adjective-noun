8317f92690a45a9bc27f2eddae9d47b88775e60a
emilybonar <emilykbonar@gmail.com>
2021-01-08 16:08:40 -0600

    Random background

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import { nounList, adjectiveList } from "./data.js";
 function App() {
 	const [adjective, setAdjective] = useState("");
 	const [noun, setNoun] = useState("");
-
+	const [backgroundColor, setBackgroundColor] = useState("indigo");
+	const colorList = [
+		"gray",
+		"red",
+		"yellow",
+		"green",
+		"blue",
+		"indigo",
+		"purple",
+		"pink",
+	];
 	useEffect(() => {
 		setAdjective(
 			adjectiveList[Math.floor(Math.random() * adjectiveList.length)],
 		);
 		setNoun(nounList[Math.floor(Math.random() * nounList.length)]);
+		setBackgroundColor(colorList[Math.floor(Math.random() * colorList.length)]);
 	}, []);
 
 	return (
-		<div className="absolute flex w-full h-full text-center text-white bg-indigo-900">
+		<div
+			className={`absolute flex w-full h-full text-center text-white bg-${backgroundColor}-900`}
+		>
 			<div className="w-5/6 m-auto">
 				<div className="text-2xl">
 					Oh, didn't you hear? She has a
