b9d459b71e9b50a9ff115fe7d9e1b4b53abfd823
emilybonar <emilykbonar@gmail.com>
2021-01-08 16:49:53 -0600

    Fixing background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	purge: {
+		content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+		options: {
+			safelist: [
+				"bg-gray-900",
+				"bg-red-900",
+				"bg-yellow-900",
+				"bg-green-900",
+				"bg-blue-900",
+				"bg-indigo-900",
+				"bg-purple-900",
+				"bg-pink-900",
+			],
+		},
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
